532047a4de000b4693fdb10443af5c4ebdd7d9ba
HannahNajihah <hannahnajihah@graduate.utm.my>
2025-06-23 05:47:55 +0800

    Fix: Excluded Exam_Score from IQR outlier removal in preprocessing

--- src/preprocess.py
+++ src/preprocess.py
@@ -53,12 +53,14 @@ def preprocess_data(path='data/student_performance.csv'):
     # Outlier removal using IQR
     numeric_cols = df.select_dtypes(include=['int64', 'float64']).columns
     for col in numeric_cols:
-        Q1 = df[col].quantile(0.25)
-        Q3 = df[col].quantile(0.75)
-        IQR = Q3 - Q1
-        lower_bound = Q1 - 1.5 * IQR
-        upper_bound = Q3 + 1.5 * IQR
-        df = df[(df[col] >= lower_bound) & (df[col] <= upper_bound)]
+     if col == 'Exam_Score':
+        continue  # Don't filter score
+     Q1 = df[col].quantile(0.25)
+     Q3 = df[col].quantile(0.75)
+     IQR = Q3 - Q1
+     lower_bound = Q1 - 1.5 * IQR
+     upper_bound = Q3 + 1.5 * IQR
+     df = df[(df[col] >= lower_bound) & (df[col] <= upper_bound)]
 
     # Print data types for inspection
     print(df.dtypes)
